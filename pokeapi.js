500243f522a8a12c0c98cbcebb009d98aab191f8
Alvaro Gracia <alvaro.gracia.reg@gmail.com>
2023-03-03 15:48:34 +0100

    App working

---
+++ pokeapi.js
@@ -0,0 +1,83 @@
+let pokeSelector = document.getElementById("pokemonselector")
+const altaPokedex = document.getElementById("altapokedex")
+const bajaPokedex = document.getElementById("bajapokedex")
+
+pokeSelector.addEventListener("change" , chargePokemon)
+
+getPokemonList()
+chargePokemon()
+
+function getPokemonList(){
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=151%22")
+    .then(response => response.json())
+    .then(data => {
+
+
+        for(let n = 0; n<152; n++){
+
+            let pokeOption = document.createElement("option")
+
+            pokeOption.textContent = "#" + (n + 1) + " " + data.results[n].name
+            pokeOption.value = n
+            pokeSelector.appendChild(pokeOption)
+        }
+    })
+}
+
+function chargePokemon(){
+
+
+    altaPokedex.innerHTML = ""
+
+    let id = pokeSelector.value
+    id++
+
+    fetch("https://pokeapi.co/api/v2/pokemon/" + id)
+    .then(response => response.json())
+    .then((data) => {
+
+        let sprite = document.createElement("img")
+        sprite.src = data.sprites.front_default
+        sprite.id = "imgid"
+        let nameP = document.createElement("p")
+        nameP.textContent = capitalFirst(data.name)
+
+        let typeP = document.createElement("p")
+
+
+
+        typeP.innerHTML = "Types: "
+        console.log(data.past_types)
+        if(data.past_types.length > 0){
+
+        data.past_types.forEach(element => {
+
+            element.types.forEach(elemnto =>{
+                typeP.innerHTML += "<span class='"+elemnto.type.name+"'>" +elemnto.type.name + "</span> "
+            
+            })
+            })
+        }else{
+            data.types.forEach(element => {
+                typeP.innerHTML += "<span class='"+element.type.name+"'>" +element.type.name + "</span> "
+                
+            })
+        }
+
+        let heigthP = document.createElement("p")
+        heigthP.textContent = "Height: " + data.height
+
+        let wheightP = document.createElement("p")
+        wheightP.textContent = "Weight: " + data.weight
+
+        altaPokedex.appendChild(sprite)
+        bajaPokedex.appendChild(nameP)
+        bajaPokedex.appendChild(typeP)
+        bajaPokedex.appendChild(heigthP)
+        bajaPokedex.appendChild(wheightP)
+})
+}
+
+function capitalFirst(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
